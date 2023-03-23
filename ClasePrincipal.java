590d57dba5893c1dfb01c8d6b9e59f004989246c
Lucas <lfeliualvarez@danielcastelao.org>
2023-03-23 23:56:00 +0100

    Commit E: edito por 2ª vez la clase principal

--- ClasePrincipal.java
+++ ClasePrincipal.java
@@ -1,10 +1,12 @@
 public class ClasePrincipal {
     public static void main(String[] args) {
         imprimirMensaje("Hola mundo!");
+        imprimirMensaje("Este es el commit E.");
     }
 
     public static void imprimirMensaje(String mensaje) {
         System.out.println(mensaje);
     }
 }
+
 
