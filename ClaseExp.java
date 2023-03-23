2842a9e659220fd4513d7774107bd850700e4ee4
Lucas <lfeliualvarez@danielcastelao.org>
2023-03-23 23:42:26 +0100

    Commit C: creo la clase Exp

---
+++ ClaseExp.java
@@ -0,0 +1,5 @@
+public class ClaseExp {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde la clase Exp");
+    }
+}
---
+++ ClasePrincipal.java
@@ -0,0 +1,10 @@
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        imprimirMensaje("Hola mundo!");
+    }
+
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
+
